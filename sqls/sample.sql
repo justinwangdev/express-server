f70fed35e99a694b1862902da3ab0ecbc3fa0c20
Justin <justin31452@gmail.com>
2020-07-22 14:17:50 +0800

    try put where at the end

--- sqls/sample.sql
+++ sqls/sample.sql
@@ -5,9 +5,9 @@ select
 from
     containerflow a,
     codetable b
+order by
+    containerno
 where
     b.code = 'H'
     and b.value = a.flowno
     and workno = '10809023'
-order by
-    containerno
