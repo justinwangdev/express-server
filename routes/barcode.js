983f171bbf04780c4c9387866c3cd95357e4244a
Justin <justin31452@gmail.com>
2020-07-22 14:50:32 +0800

    add "

--- routes/barcode.js
+++ routes/barcode.js
@@ -25,15 +25,16 @@ router.all('/test', function(req, res, next) {
         .then(response => response.text())
         .then(text => {
             var sql = text;
-            var where = "where containerflow.workno = ";
+            var where = "where containerflow.workno = \"";
             var tmp = req.body.workno;
             where = where.concat(tmp);
-            where = where.concat(" and containerflow.containerno = ");
+            where = where.concat("\" and containerflow.containerno = \"");
             tmp = req.body.containerno;
             where = where.concat(tmp);
-            where = where.concat(" and containerflow.flowno = ");
+            where = where.concat("\" and containerflow.flowno = \"");
             tmp = req.body.procedureCode;
             where = where.concat(tmp);
+            where = where.concat("\"");
             sql = sql.concat(where);
             connection.getConnection(function(err, connection) {
                 connection.query(sql, function(error, results, fields) {
