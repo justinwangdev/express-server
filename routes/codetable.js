817bb3ef52d3fd1ef1b333d106935d60f628d9f6
Justin Wang <justin31452@gmail.com>
2020-07-22 10:26:35 +0800

    init

---
+++ routes/codetable.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var router = express.Router();
+
+const connection = mysql.createPool({
+    host: '192.168.1.123',
+    user: 'justin',
+    password: '22563952',
+    database: 'mhs'
+});
+
+router.get('/', function(req, res, next) {
+    // Connecting to the database.
+    connection.getConnection(function(err, connection) {
+
+        // Executing the MySQL query (select all data from the 'users' table).
+        connection.query("select Name, Value from codetable where Code='H' and Tag='0YY' order by Value;", function(error, results, fields) {
+            // If some error occurs, we throw an error.
+            if (error) throw error;
+
+            // Getting the 'response' from the database and sending it to our route. This is were the data is.
+            res.send(results)
+        });
+    });
+});
+
+module.exports = router;
