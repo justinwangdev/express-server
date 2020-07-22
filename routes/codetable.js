8da8ddef788f33d591044b2f65e42e189cfed6cc
justin31452 <43922308+justin31452@users.noreply.github.com>
2020-07-22 10:32:47 +0800

    add a post method "test"

--- routes/codetable.js
+++ routes/codetable.js
@@ -25,4 +25,9 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.post('/test', function(req, res, next) {
+    res.send('respond with a resource');
+    console.log(req.body);
+});
+
 module.exports = router;
