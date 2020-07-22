9db0d855c3072a4990b044811cf2ad8519d463fb
Justin <justin31452@gmail.com>
2020-07-22 10:47:07 +0800

    testing git

--- routes/codetable.js
+++ routes/codetable.js
@@ -27,6 +27,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/test', function(req, res, next) {
     console.log(req.body);
+    console.log(req.body.weight);
 });
 
 module.exports = router;
